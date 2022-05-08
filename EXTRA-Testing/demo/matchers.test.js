76052d4d22b73c7773a6ff2af2d0eed58197cc7b
miguelgallardof <miguel.gallardof@icloud.com>
2022-05-08 03:09:19 -0500

    homework Testing

--- EXTRA-Testing/demo/matchers.test.js
+++ EXTRA-Testing/demo/matchers.test.js
@@ -5,9 +5,11 @@ xdescribe('Tests that will pass', () => {
     expect(number).toBe(3);
     expect(string).toBe('Franco');
   });
+  // toEqual verifica recursivamente cada campo de un objeto
+  // o de un arreglo.
   it('should pass if recursively match (object)', () => {
     const obj = {name: 'Franco', age: 27};
-    expect(obj).toEqual({name: 'Franco', age: 27});
+    expect(obj).toBe({name: 'Franco', age: 27});
   });
   
   it('should pass if recursively match (array)', () => {
@@ -28,19 +30,22 @@ xdescribe('Tests that will not pass', () => {
   });
 })
 
-xdescribe('Extra matchers', () => {
+describe('Extra matchers', () => {
 
     it('toBeNull', () => {
+    // toBeNull coincide solo con null
       expect(null).toBeNull();
       // expect(undefined).toBeNull();
     });
   
     it('toBeUndefined', () => {
+    // toBeUndefined coincide solo con undefined 
       expect(undefined).toBeUndefined();
       // expect(null).toBeUndefined();
     });
 
     it('toBeDefined', () => {
+    // toBeDefinedes el opuesto a toBeUndefined  
       expect(2).toBeDefined();
       expect(0).toBeDefined();
       expect('').toBeDefined();
@@ -53,7 +58,9 @@ xdescribe('Extra matchers', () => {
       // expect(undefined).toBeDefined();
     });
 
-    // toBeFalsy --> opposite
+   // toBeFalsy coincide con lo que sea que el condicional if devuelva como falso
+   // toBeTruthy coincide con lo que sea que el condicional if devuelva como verdadero  
+   // toBeFalsy --> opposite
     it('toBeTruthy', () => {
       expect(true).toBeTruthy();
       expect(2).toBeTruthy();
@@ -67,21 +74,24 @@ xdescribe('Extra matchers', () => {
       // expect(undefined).toBeTruthy();
       // expect(null).toBeTruthy();
     });
-
+// Para la igualdad de puntos flotantes, usa toBeCloseTo en lugar de toEqual, 
+// porque no quiere que una prueba dependa de un pequeño error de redondeo.
+// Utilícelo para comparar números de coma flotante para una igualdad aproximada
+//  3 * 10.3; //el resultado debería ser 30.9 //pero JS da 30.900000000000002
     it('toBeCloseTo', () => {
       // expect(0.1 + 0.2).toEqual(0.3);
       expect(0.1 + 0.2).toBeCloseTo(0.3);
       // expect(0.11).toBeCloseTo(0.1);
-      expect(0.11).toBeCloseTo(0.1, 1);
+      expect(0.11).toBeCloseTo(0.1, 2);
       expect(0.174927142).toBeCloseTo(0.17492, 4);
     });
-
+    // Puede probar cadenas contra expresiones regulares con toMatch
     it('toMatch', () => {
       expect('Un texto largo que tiene Franco dentro de sus palabras').toMatch('Franco');
       expect('Un texto largo que tiene Franco dentro de sus palabras').toMatch(/Franco/);
       expect('Un texto largo que tiene Franco dentro de sus palabras').toMatch(/palabras$/);
     });
-
+    // Puedes comprobar si un array o iterable contiene un elemento en particular usando toContain
     it('toContain', () => {
       const array = ['Franco', 'Toni', 'Martu', 'Mati'];
       expect(array).toContain('Martu');
@@ -89,8 +99,11 @@ xdescribe('Extra matchers', () => {
 
       expect('Soy Henry').toContain('Henry');
     });
-
-    // also used as .toThrowError
+  // Si quiere probar que si una función particular lanza un error al llamarse, use toThrow
+  // la función que arroja una excepción necesita ser invocada dentro de una función que la 
+  // envuelva, de lo contrario la afirmación toThrow fallará.
+  // also used as .toThrowError
+  // me permite ver si la funcion arroja el error que yo quiero que muestre.
     it('toThrow', () => {
       function error() {
         throw new TypeError('An error');
---
+++ EXTRA-Testing/homework/tests/index.test.js
@@ -0,0 +1,74 @@
+/*it("Always true", () => {
+  expect(true).toBe(true);
+});*/
+const { checkSeatStatus, getRowNumber, book } = require("../homework");
+
+describe("checkSeatStatus", () => {
+  it("checkSeatStatus is a function", () => {
+    expect(typeof checkSeatStatus).toBe("function");
+  });
+
+  it("should throw a TypeError if first parameter is not a string", () => {
+    expect(() => checkSeatStatus(4)).toThrow(
+      new TypeError("First parameter is not a string")
+    );
+  });
+
+  it("should throw a TypeError if second parameter is not a number", () => {
+    expect(() => checkSeatStatus("A", "2")).toThrow(
+      new TypeError("Second parameter is not a number")
+    );
+  });
+  it("should return true if the seat is booked", () => {
+    expect(checkSeatStatus("A", 1)).toBe(true);
+  });
+
+  it("should return false if the seat is not booked", () => {
+    expect(checkSeatStatus("E", 3)).toBe(false);
+  });
+
+  it("should throw a TypeError if the parameter is a word", () => {
+    expect(() => checkSeatStatus("Hola", 1)).toThrow(
+      new TypeError("This parameter is not a letter")
+    );
+  });
+
+  it("should throw a TypeError if the parameter is an empty string", () => {
+    expect(() => checkSeatStatus("", 1)).toThrow(
+      new TypeError("This parameter is an empty string")
+    );
+  });
+
+  it("should throw a TypeError if the parameter is greater than 4", () => {
+    expect(() => checkSeatStatus("A", 4)).toThrow(
+      new TypeError("This parameter is greater than 4")
+    );
+  });
+});
+
+describe("getRowNumber", () => {
+  it("should return 0 if the letter given is an A", () => {
+    expect(getRowNumber("A")).toBe(0);
+  });
+
+  it("should return 2 if the letter given is a C", () => {
+    expect(getRowNumber("C")).toBe(2);
+  });
+});
+
+describe("book", () => {
+  it("should return true if the seat is not booked", () => {
+    expect(book("E", 2)).toBe("Seat in E2 successfully booked");
+  });
+
+  it("should return true if the seat is booked", () => {
+    expect(book("A", 1)).toBe("Seat in A1 is already booked");
+  });
+
+  it('should return "Seat in XX successfully booked" if the given seat is not booked', () => {
+    expect(checkSeatStatus("E", 3)).toBe(false);
+    expect(book("E", 3)).toBe("Seat in E3 successfully booked");
+    expect(checkSeatStatus("E", 3)).toBe(true);
+    expect(book("E", 3)).toBe("Seat in E3 is already booked");
+  });
+});
